b2246a4c799edeb1c7139acc7c4f1d14eb92ed3b
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-04 16:29:26 +0300

    add books

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext } from "react";
+import "./styles.css";
+import { Route } from "react-router-dom";
 
-function App() {
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+
+import { data } from "./data";
+
+export const BooksContext = createContext();
+
+export default function App() {
   return (
+    <BooksContext.Provider value={data}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Alışveriş Sepeti Yapımı
+        <img
+          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
+          alt="React Dersleri"
+        />{" "}
+        React Dersleri
+      </h1>
+      <Route exact path="/" component={Products} />
+      <Route path="/cart" component={Cart} />
     </div>
+    </BooksContext.Provider>
   );
 }
-
-export default App;
